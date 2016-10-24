f6fd93e359797ddd43f22e0141a511a5f1fde411
Wellming Li <wellming.li@howzhi.com>
2016-10-24 09:35:58 +0800

    clean code

--- app/Resources/assets/global.js
+++
@@ -1,5 +0,0 @@
-$(document).ajaxSend(function(a, b, c) {
-    if (c.type == 'POST') {
-        b.setRequestHeader('X-CSRF-Token', $('meta[name=csrf-token]').attr('content'));
-    }
-});
--- src/Biz/User/Dao/UserDao.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Biz\User\Dao;
-
-use Codeages\Biz\Framework\Dao\GeneralDaoInterface;
-
-interface UserDao extends GeneralDaoInterface
-{
-    public function getByUsername($username);
-}
